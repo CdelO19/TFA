7785492ab21d60d51bf2d0e5f049137531af1b75
Celia <delanois.celia@gmail.com>
2022-08-18 12:57:23 +0200

    mise en place du json

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,13 @@
 import * as burger from 'burger.js';
 
-
-
+fetch('../donnes.json')
+.then(
+    (response) => {
+        console.log(response.json());
+    },
+    (response) => {
+        console.log("une erreur est survenue.");
+    }
+);
 
 
